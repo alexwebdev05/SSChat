acd76771a3faf27f31434c8d4821ad46a9269077
alexwebdev05 <alexfriasangulo@gmail.com>
2024-12-02 08:30:13 +0100

    getMessage finished

--- api/src/controllers/message.controller.ts
+++ api/src/controllers/message.controller.ts
@@ -10,7 +10,7 @@ export class MessageController {
     // Get messages
     static async getmessages(req: Request, res: Response): Promise<void> {
         try {
-            const data: IMessage = req.body
+            const data = req.body
             const messageData = await MessageModel.getMessages(data)
             res.status(201).json(messageData)
         } catch(error) {
--- api/src/models/message.model.ts
+++ api/src/models/message.model.ts
@@ -5,8 +5,27 @@ import { dbConnect } from "../conf/db";
 export class MessageModel {
 
     // Get messages
-    static async getMessages(data: IMessage): Promise<void> {
-        const { sender, receiver } = data
+    static async getMessages(data: Omit<IMessage, 'message'> & {user: string}) {
+        const client = await dbConnect();
 
+        try {
+            
+            const sender = data.sender
+            const receiver = data.receiver
+
+            const response = await client.query(
+                'SELECT * FROM messages WHERE (sender = $1 OR receiver = $1) AND (sender = $2 OR receiver = $2);',
+                [sender, receiver]
+            )
+
+            return response.rows
+
+        } catch (error) {
+
+            console.log('[ SERVER ] Failed to get messages at model: ' + error)
+
+        } finally {
+            await client.end();
+        }
     }
 }
